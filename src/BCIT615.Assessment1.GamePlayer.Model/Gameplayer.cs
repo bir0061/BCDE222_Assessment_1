01464cbcc0d0c5fe62ce369f04b6497efe18a23d
Biken Rai [bir0061] <bir0061@arastudent.ac.nz>
2026-09-07 13:20:03 +1200

    Intial Dervelopment

---
+++ src/BCIT615.Assessment1.GamePlayer.Model/Gameplayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCIT615.Assessment1.GamePlayer.Model
+{
+    internal class Gameplayer
+    {
+    }
+}
